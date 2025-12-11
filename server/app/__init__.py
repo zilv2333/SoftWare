4046ef126a0924982f5d09fe5ea238bb2edb7d45
XYC <2383550871@qq.com>
2025-12-11 23:53:07 +0800

    '上传目录自动创建'

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,3 +1,4 @@
+import os
 
 
 def create_app():
@@ -60,6 +61,9 @@ def create_app():
     get_db()
 
     # ... 其他配置（如数据库）
+    os.makedirs(config.VIDEO_FOLDER, exist_ok=True)
+    os.makedirs(config.THUMBNAIL_FOLDER,exist_ok=True)
+    os.makedirs(config.UPLOAD_FOLDER,exist_ok=True)
 
     CORS(app)  # 启用CORS支持[1,4](@ref)
     return app
